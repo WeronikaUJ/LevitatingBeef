f5c6dcade3a950590f57be063955955a1db9e1f1
WeronikaUJ <64475136+WeronikaUJ@users.noreply.github.com>
2020-06-10 09:00:46 +0200

    Create Actor.hpp

---
+++ Actor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+class Actor : public sf::Drawable
+{
+public:
+	Actor();
+	Actor(const Actor&) = default;
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+		target.draw(sprite, states);
+	}
+	virtual sf::Sprite& getSprite() {
+		return sprite;
+	}
+	virtual void Delete()  {
+		erase = true;
+	}
+
+	virtual void update() = 0;
+
+	virtual bool shouldBeErase() const {
+		return erase;
+	}
+  
+  virtual void onCollision(Actor* collider) {};
+
+	virtual void setTag(const std::string& t) {
+		tag = t;
+	}
+	virtual const std::string& getTag() const {
+		return tag;
+	}
+protected:
+	std::string tag;
+	bool erase;
+	sf::Sprite sprite;
+};
